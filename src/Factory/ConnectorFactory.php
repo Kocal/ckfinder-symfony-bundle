2f877626ed503ce1d2859712264e365805674369
Hugo Alliaume <halliaume@wamiz.com>
2022-08-27 15:49:37 +0200

    chore(ecs): migrate to new ECS config

--- src/Factory/ConnectorFactory.php
+++ src/Factory/ConnectorFactory.php
@@ -29,7 +29,9 @@ class ConnectorFactory
         /** @var CKFinder $connector */
         $connector = new $this->connectorConfig['connectorClass']($this->connectorConfig);
         $connector['authentication'] = $this->authenticationService;
-        $connector['services_map'] = fn (): \Psr\Container\ContainerInterface => $this->servicesMap;
+        $connector['services_map'] = function () {
+            return $this->servicesMap;
+        };
 
         return $this->connectorInstance = $connector;
     }
