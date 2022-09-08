1b35bfb67a30b37147370225eb56e33435280101
Hugo Alliaume <hugo@alliau.me>
2022-09-08 12:10:33 +0200

    fix(patcher): don't patch Psr scoped dependencies

--- src/Patcher/ScopedDependenciesPatcher.php
+++ src/Patcher/ScopedDependenciesPatcher.php
@@ -18,14 +18,11 @@ class ScopedDependenciesPatcher implements PatcherInterface
             ->in($connectorPath)
             ->contains([
                 'use League\\', ' \\League',
-                'use Psr\\', ' \\Psr',
             ]);
 
         foreach ($files as $file) {
             $this->patchFile($file, 'use League\\', 'use \\' . self::PREFIX . '\\League\\', false);
             $this->patchFile($file, ' \\League\\', ' \\' . self::PREFIX . '\\League\\', false);
-            $this->patchFile($file, 'use Psr\\', 'use \\' . self::PREFIX . '\\Psr\\', false);
-            $this->patchFile($file, ' \\Psr\\', ' \\' . self::PREFIX . '\\Psr\\', false);
         }
     }
 }
